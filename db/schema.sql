3da233fbe76fd49fff79189a4501c5575bce9912
Luka Licheli <lukalicheli@gmail.com>
2022-02-01 16:19:10 -0800

    Updated seeds

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE deptRole (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT NOT NULL,
+    department_id INT
     FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
@@ -21,10 +21,10 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30), 
     last_name VARCHAR(30), 
-    role_id INT NOT NULL,
+    role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES deptRole(id),
-    manager_id INT NOT NULL, 
+    manager_id INT, 
     FOREIGN KEY (manager_id)
     REFERENCES employee(manager_id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,25 @@
-INSERT INTO department_name (dept_name)
+INSERT INTO department (dept_name)
 VALUES ("IT"),
        ("Sales"),
        ("Marketing"),
        ("Accounting");
+
+INSERT INTO deptRole (title, salary, department_id)
+VALUES ("Hardware Technician"),
+       ("Help Desk Support"),
+       ("Sales Lead"),
+       ("Salesperson"),
+       ("Chief Marketing Officer"), 
+       ("Creative Director"), 
+       ("Bookkeeper"), 
+       ("Accounting Manager");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Luka", "Licheli", 1, NULL),
+       ("Steve", "Brown", 2, 001),
+       ("Gina", "Lopez", 3, 002),
+       ("John", "Connor", 4, NULL), 
+       ("Vero", "Martinez", 5, 001),
+       ("Vero", "Martinez", 6, 002), 
+       ("Vero", "Martinez", 7, 001), 
+       ("Vero", "Martinez", 8, 002),  
